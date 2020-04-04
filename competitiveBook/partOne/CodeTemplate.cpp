0c3ad8f8ddfe6a827108fabb2e9a3758452613f0
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2020-04-04 13:16:42 +0200

    Adding code template to the ifle

---
+++ competitiveBook/partOne/CodeTemplate.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//The standard command that we are going to use to execute a file in cpp
+//is "g++ -std=c++11 -02 -Wall file.cpp -o file"
+//The param -std=c++11 will say to the compiler to compile the file in c++
+//The param -02 will be use to optimize the result of the compilation
+//The param -Wall is used to warnings
+//The param -o is to output the executable file with the name that you write there.
+int main() {
+    // Solution comes here
+    return 0;
+}
