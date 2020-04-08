79cf8d2de89912b59dabe568eb638dd52366b2e8
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2020-04-08 18:06:25 +0200

    Adding some stuff

---
+++ problems/leetcode/2-Add Two Numbers/java/Solution.java
@@ -0,0 +1,44 @@
+
+import java.util.Scanner;
+
+public class Solution {
+
+	private static final Scanner scan = new Scanner(System.in);
+
+	public static void main(final String... args) {
+		
+		int a, b;
+
+		a = scan.nextInt();
+		b = scan.nextInt();
+
+		ListNode l = 
+
+		ListNode head = addTwoNumbers();
+
+		scan.close();
+
+	}
+
+	static ListNode fromIntToListNode(int n) {
+		ListNode head = new ListNode(n % 10);
+	}
+
+	static ListNode addTwoNumbers(ListNode l, ListNode ll) {
+
+	}
+
+	static class ListNode {
+		
+		int val;
+
+		ListNode next;
+
+		public ListNode(int val) {
+			this.val = val;
+		}
+
+	}
+
+}
+
