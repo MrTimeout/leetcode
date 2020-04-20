b5954e7f8eb32d22e315f9243ba9e4529596023b
estenoesmiputonombre <49493249+estenoesmiputonombre@users.noreply.github.com>
2020-04-21 00:12:07 +0200

    Update structs.rs

--- Rust/Learning/structs.rs
+++ Rust/Learning/structs.rs
@@ -1,39 +1,66 @@
 
 struct Message {
-  from: Person,
-  to: Person,
-  content: String,
+	id: u32,
+	from: Person,
+	to: Person,
+	content: String,
+}
+
+impl Message {
+	fn new(id: u32, from: Person, to: Person, content: String) -> Message {
+		Message { id: id, from: from, to: to, content: content }
+	}
 }
 
 struct Person {
-  id: u32,
-  name: String,
+	id: u32,
+	name: String,
+}
+
+impl Person {
+	fn new(id: u32, name: String) -> Person {
+		Person { id: id, name: name }
+	}
 }
 
 fn get_users() -> Vec<Person> {
-  let mut users: Vec<Person> = vec![];
-  let names: Vec<String> = vec![
-    String::from("Noemi"),
-    String::from("Ivan")
-  ];
-  let mut cont: u32 = 0;
-  for i in &names {
-    cont += 1;
-    users.push(Person {
-      id: cont,
-      name: i.to_string(),
-    });
-  }
-  users
+	let mut users: Vec<Person> = vec![];
+	let names: Vec<String> = vec![
+		String::from("aaa"),
+		String::from("bbb")
+	];
+	let mut cont: u32 = 0;
+	for i in &names {
+		cont += 1;
+		users.push(Person::new(cont, i.to_string()));
+	}
+	users
 }
 
-fn send_message() -> bool {
+struct MessageFactory {
+	id: u32,
+}
 
+impl MessageFactory {
+	fn increment_and_get(&mut self) -> u32 {
+		self.id += 1;
+		self.id
+	}
+	fn send(&mut self, content: String, from: &Person, to: &Person) -> bool {
+		println!("Sending a message from {} to {}", from.name, to.name);
+		println!("id: {}, Content: {}", self.increment_and_get(), content);
+		true
+	}
+	fn new() -> MessageFactory {
+		MessageFactory{ id: 0 }
+	}
 }
 
 fn main() {
-  let users: Vec<Person> = get_users();
-  for i in &users {
-  	println!("Name: {}, Id: {}", i.id, i.name);
-  }
-}}
+	let users: Vec<Person> = get_users();
+	for i in &users {
+		println!("Name: {}, Id: {}", i.id, i.name);
+	}
+	let mut message_factory: MessageFactory = MessageFactory::new();
+	message_factory.send("Let's play".to_string(), &users[0], &users[1]);
+}
